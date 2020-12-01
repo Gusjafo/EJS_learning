91178975d9d7e88c5c75142d7b5fbaeb5ffee4b1
Gusjafo <gustavojafo@gmail.com>
2020-12-01 20:35:27 -0300

    segundo_cambio

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const bodyparser = require('body-parser');
 const app = express();
 
 app.get.('/', function(req, res){
+
+  var today = new Date();
+  var currentDay = today.getDay();
+  console.log(currentDay);
   res.send('Hello World');
 });
 
