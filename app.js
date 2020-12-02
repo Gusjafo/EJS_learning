3bafae8cae4a2f79433007695eb3ef9e34aa309a
Gusjafo <gustavojafo@gmail.com>
2020-12-02 12:46:36 -0300

    new branch

--- app.js
+++ app.js
@@ -8,39 +8,11 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
 
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: " + currentDay);
-  }
-  console.log(currentDay);
-  console.log(day);
+  var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var currentDay = day[today.getDay()];
 
   res.render('lists', {
-    kindOfDay: day
+    kindOfDay: currentDay
   });
 });
 
