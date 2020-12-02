7430ae561e5f9d2cf76040c09010e4bad8dde6a3
Gusjafo <gustavojafo@gmail.com>
2020-12-02 11:43:22 -0300

    My third commit

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const bodyparser = require('body-parser');
 
 const app = express();
 
-app.get.('/', function(req, res){
+app.get('/', function(req, res){
 
   var today = new Date();
   var currentDay = today.getDay();
   console.log(currentDay);
   res.send('Hello World');
+
 });
 
 app.listen(3000);
