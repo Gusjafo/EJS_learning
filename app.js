509ac31498b1ec47a84f4243ef8752d821ae0189
Gusjafo <gustavojafo@gmail.com>
2020-12-02 12:36:48 -0300

    Case switch

--- app.js
+++ app.js
@@ -3,13 +3,45 @@ const bodyparser = require('body-parser');
 
 const app = express();
 
-app.get('/', function(req, res){
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
 
   var today = new Date();
   var currentDay = today.getDay();
-  console.log(currentDay);
-  res.send('Hello World');
+  var day = "";
 
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to: " + currentDay);
+  }
+  console.log(currentDay);
+  console.log(day);
+
+  res.render('lists', {
+    kindOfDay: day
+  });
 });
 
 app.listen(3000);
