4cc5dd190bec03c484d37032588d8a0f64ca45cc
Gusjafo <gustavojafo@gmail.com>
2020-12-07 10:15:33 -0300

    add item list in new line

--- app.js
+++ app.js
@@ -2,20 +2,39 @@ const express = require('express');
 const bodyparser = require('body-parser');
 
 const app = express();
+let tasks = ["asleep", "take a breakfast", "study web development"];
 
 app.set('view engine', 'ejs');
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function(req, res) {
 
-  var today = new Date();
-  var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  var currentDay = day[today.getDay()];
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("es-LA", options);
 
   res.render('lists', {
-    kindOfDay: currentDay
+    kindOfDay: day,
+    newListTask: tasks
   });
+
 });
 
-app.listen(3000, function(){
+app.post('/', function(req, res) {
+  task = req.body.newTask;
+  tasks.push(task);
+
+  res.redirect("/");
+})
+
+app.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
