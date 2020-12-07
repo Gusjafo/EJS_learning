6a6a67ed74d53f48fe691dc64b7e0eb6e0e7f072
Gusjafo <gustavojafo@gmail.com>
2020-12-07 18:18:52 -0300

    progress

--- app.js
+++ app.js
@@ -3,18 +3,38 @@ const bodyparser = require('body-parser');
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
 
   var today = new Date();
-  var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  var currentDay = day[today.getDay()];
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric"
+  };
+
+
+  var day = today.toLocaleDateString("es-LA", options);
 
   res.render('lists', {
-    kindOfDay: currentDay
+    kindOfDay: day
   });
 });
+
+app.post('/', function(req, res){
+  var task = req.body.newTask;
+  console.log(task);
+})
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000");
