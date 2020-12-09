28caf174175f258de74449addb808637c0e5da3e
Gusjafo <gustavojafo@gmail.com>
2020-12-09 07:39:10 -0300

    Forma simple de agregar nuevos templates "similares"

--- app.js
+++ app.js
@@ -2,46 +2,55 @@ const express = require('express');
 const bodyparser = require('body-parser');
 
 const app = express();
-let tasks = ["asleep", "take a breakfast", "study web development"];
+let tasksArray = ["asleep", "take a breakfast", "study web development"];
+let workItems = [];
 
 app.use(express.static('public'));
 app.use(bodyparser.urlencoded({
   extended: true
 }));
-
 app.set('view engine', 'ejs');
-app.use(bodyparser.urlencoded({
-  extended: true
-}));
+
 
 app.get('/', function(req, res) {
-
   let today = new Date();
-
   let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
-
   let day = today.toLocaleDateString("es-LA", options);
-
-  res.render('lists', {
-    kindOfDay: day,
-    newListTask: tasks
-
+  res.render('index', {
+    listTitle: day,
+    newListTask: tasksArray
   });
-
 });
 
 app.post('/', function(req, res) {
-  task = req.body.newTask;
-  tasks.push(task);
+  let task = req.body.newTask;
 
-  res.redirect("/");
+  if(req.body.list === "Trabajo") {
+    workItems.push(task);
+    res.redirect("/work");
+  }else {
+    tasksArray.push(task);
+    res.redirect("/");
+  }
+});
+
+app.get('/work', function(req, res) {
+  res.render("index", {
+    listTitle: "Trabajo",
+    newListTask: workItems
+  });
+});
+
+app.post('/work', function(req, res){
+  let item = req.body.newTask;
+  workItems.push(item);
+  res.redirect("/work");
 })
 
 app.listen(3000, function() {
-
   console.log("Server is running on port 3000");
 });
